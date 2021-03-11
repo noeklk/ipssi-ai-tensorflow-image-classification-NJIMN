535ee8da5b075ca10bf33cdd5a3fb944e2d2b559
feinglong <feinglong@gmail.com>
2021-03-11 18:34:04 +0100

    add uploaded file function

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
 
 # File Processing Pkgs
 from PIL import Image
@@ -7,30 +10,40 @@ from PIL import Image
 def load_image(image_file):
     img = Image.open(image_file)
     return img
+
+def display_img(img):
+    imgplot = plt.imshow(img)
+    imgplot = plt.yticks([])
+    imgplot = plt.xticks([])
+    return imgplot
+    
+
+def load_img_bis(uploaded_file):
+    # Convert the file to an opencv image.
+    cv2.ocl.setUseOpenCL(False)
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes, 1)
+    cv_rgb = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2GRAY)
+    return cv_rgb, gray
     
     
 def main():
-    st.title("My File upload app")
+    st.title("Mini app")
     
     menu =["Home", "Dataset" , "DocumentFiles", "About"]
     choice = st.sidebar.selectbox("Menu", menu)
     
     if choice == "Home":
         st.subheader("Home")
-        image_file = st.file_uploader("Upload Images" , type=["png", "jpg" , "jpeg"])
-        if image_file is not None:
-            # To see details
-            st.write(type(image_file))
-            # Methods & Attributs
-            # st.write(dir(image_file))
-            file_details = {"filename" : image_file.name,
-                            "filetype" : image_file.type,
-                            "filesize" : image_file.size}
-            st.write(file_details)
-            
-            st.image(load_image(image_file),width=250)
-    
-
         
+        image_file = st.file_uploader("Upload Image", type=["png", "jpg" , "jpeg"])
+        if image_file is not None:
+            cv_rgb , gray = load_img_bis(image_file)
+            st.image(cv_rgb)
+            st.image(gray)
+
+            
+            
 if __name__ == '__main__':
     main()
