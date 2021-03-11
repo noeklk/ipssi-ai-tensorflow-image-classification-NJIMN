55a0f8882371f92a046803f2ced5a694a6b2f823
feinglong <feinglong@gmail.com>
2021-03-12 00:30:42 +0100

    normalisation des noms des images

--- app/app.py
+++ app/app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
+import re
 import os 
+import glob
 
 # File Processing Pkgs
 from PIL import Image
@@ -28,16 +30,29 @@ def load_img_bis(uploaded_file):
     gray = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2GRAY)
     return cv_rgb, gray
     
-def save_img(uploaded_file, path):
+def save_img(uploaded_file, dogs_breed):
+    # Path 
+    dogs_breed = str(dogs_breed)
+    path = "data/"+dogs_breed
     # Test : Verifie si le dossier existe sinon il le
     try: 
         os.makedirs(path)
     except OSError:
         if not os.path.isdir(path):
             raise
-    with open(os.path.join(path , uploaded_file.name),"wb") as f: 
+    # Normaliser les noms des fihiers
+    file_type = "jpg"
+    # modele = n02085620_477.jpg
+    # nom du dossier = n02085620_Chihuahua
+    firstname = format_race(dogs_breed)
+    lastname = len(glob.glob(os.path.join(path, '*'))) + 1
+    final_name = firstname+"_"+ str(lastname)
+    with open(os.path.join(path , final_name +"." +file_type),"wb") as f: 
         f.write(uploaded_file.getbuffer())
         st.success("Saved File")
+
+def format_race(race):
+    return re.search(r'^.*?(?=-)', race).group(0)
     
     
 def main():
@@ -50,27 +65,19 @@ def main():
         st.subheader("Home")
         image_file = st.file_uploader("Upload Image", type=["png", "jpg" , "jpeg"])
         
-        
         # TEST : on suppose qu'il prevoit que le chien est un chihuahua
         # To do: récupérer de facon dynamique la race du chien
         dogs_breed = "n02085620-Chihuahua"
-        
-        # Path 
-        path = "data/"+dogs_breed
+    
 
         if image_file is not None:
             cv_rgb , gray = load_img_bis(image_file)
             st.image(cv_rgb)
-            st.write(image_file.name)
-            
+            st.write(image_file.type)
+            # st.write(dir(image_file))
             
             if st.button('Enregister? '):
-                save_img(image_file, path)
-                
-                
-                
-                
-            
-            
+                save_img(image_file, dogs_breed)
+
 if __name__ == '__main__':
     main()
