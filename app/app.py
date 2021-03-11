00b6fb6e658fb4b8ad4d76c7f4318ca8cd9b1e9a
feinglong <feinglong@gmail.com>
2021-03-11 21:06:32 +0100

    ajout de la fonction save file

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
+import os 
 
 # File Processing Pkgs
 from PIL import Image
@@ -27,6 +28,17 @@ def load_img_bis(uploaded_file):
     gray = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2GRAY)
     return cv_rgb, gray
     
+def save_img(uploaded_file, path):
+    # Test : Verifie si le dossier existe sinon il le
+    try: 
+        os.makedirs(path)
+    except OSError:
+        if not os.path.isdir(path):
+            raise
+    with open(os.path.join(path , uploaded_file.name),"wb") as f: 
+        f.write(uploaded_file.getbuffer())
+        st.success("Saved File")
+    
     
 def main():
     st.title("Mini app")
@@ -36,13 +48,28 @@ def main():
     
     if choice == "Home":
         st.subheader("Home")
-        
         image_file = st.file_uploader("Upload Image", type=["png", "jpg" , "jpeg"])
+        
+        
+        # TEST : on suppose qu'il prevoit que le chien est un chihuahua
+        # To do: récupérer de facon dynamique la race du chien
+        dogs_breed = "n02085620-Chihuahua"
+        
+        # Path 
+        path = "data/"+dogs_breed
+
         if image_file is not None:
             cv_rgb , gray = load_img_bis(image_file)
             st.image(cv_rgb)
-            st.image(gray)
-
+            st.write(image_file.name)
+            
+            
+            if st.button('Enregister? '):
+                save_img(image_file, path)
+                
+                
+                
+                
             
             
 if __name__ == '__main__':
